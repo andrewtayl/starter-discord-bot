0e5d721aaafb2cb7f629688b191bb22aba5b16e8
Andrew Taylor <39216820+andrewtayl@users.noreply.github.com>
2022-10-21 22:42:37 -0400

    main1

--- index.js
+++ index.js
@@ -1,114 +1,113 @@
-require('dotenv').config();
-const APPLICATION_ID = process.env.APPLICATION_ID;
-const TOKEN = process.env.TOKEN;
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set';
-const GUILD_ID = process.env.GUILD_ID;
 
-import { config } from 'dotenv';
-import {  ActionRowBuilder,  Client,  GatewayIntentBits,  InteractionType,  ModalBuilder,  Routes,  SelectMenuBuilder,  TextInputBuilder,  TextInputStyle,} from 'discord.js';
-import { REST } from '@discordjs/rest';
-import OrderCommand from './commands/order.js';
-import RolesCommand from './commands/roles.js';
-import UsersCommand from './commands/user.js';
-import ChannelsCommand from './commands/channel.js';
-import BanCommand from './commands/ban.js';
-import RegisterCommand from './commands/register.js';
+// const { clientId, guildId, token, publicKey } = require('./config.json');
+require('dotenv').config()
+const APPLICATION_ID = process.env.APPLICATION_ID 
+const TOKEN = process.env.TOKEN 
+const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
+const GUILD_ID = process.env.GUILD_ID 
 
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+const axios = require('axios')
+const express = require('express');
+const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
 
-client.on('ready', () => console.log(`${client.user.tag} has logged in!`));
+const app = express();
+// app.use(bodyParser.json());
 
-client.on('interactionCreate', (interaction) => {
-  if (interaction.isChatInputCommand()) {
-    console.log('Chat Command');
-    if (interaction.commandName === 'order') {
-      const actionRowComponent = new ActionRowBuilder().setComponents(
-        new SelectMenuBuilder().setCustomId('food_options').setOptions([
-          { label: 'Cake', value: 'cake' },
-          { label: 'Pizza', value: 'pizza' },
-          { label: 'Sushi', value: 'sushi' },
-        ])
-      );
-      const actionRowDrinkMenu = new ActionRowBuilder().setComponents(
-        new SelectMenuBuilder().setCustomId('drink_options').setOptions([
-          { label: 'Orange Juice', value: 'orange_juice' },
-          { label: 'Coca-Cola', value: 'coca_cola' },
-        ])
-      );
-      interaction.reply({
-        components: [actionRowComponent.toJSON(), actionRowDrinkMenu.toJSON()],
-      });
-    } else if (interaction.commandName === 'register') {
-      const modal = new ModalBuilder()
-        .setTitle('Register User Form')
-        .setCustomId('registerUserModal')
-        .setComponents(
-          new ActionRowBuilder().setComponents(
-            new TextInputBuilder()
-              .setLabel('username')
-              .setCustomId('username')
-              .setStyle(TextInputStyle.Short)
-          ),
-          new ActionRowBuilder().setComponents(
-            new TextInputBuilder()
-              .setLabel('email')
-              .setCustomId('email')
-              .setStyle(TextInputStyle.Short)
-          ),
-          new ActionRowBuilder().setComponents(
-            new TextInputBuilder()
-              .setLabel('comment')
-              .setCustomId('comment')
-              .setStyle(TextInputStyle.Paragraph)
-          )
-        );
-
-      interaction.showModal(modal);
-    }
-  } else if (interaction.isSelectMenu()) {
-    console.log('Select Menu');
-    if (interaction.customId === 'food_options') {
-      console.log(interaction.component);
-    } else if (interaction.customId === 'drink_options') {
-    }
-  } else if (interaction.type === InteractionType.ModalSubmit) {
-    console.log('Modal Submitted...');
-    if (interaction.customId === 'registerUserModal') {
-      console.log(interaction.fields.getTextInputValue('username'));
-      interaction.reply({
-        content: 'You successfully submitted your details!',
-      });
-    }
+const discord_api = axios.create({
+  baseURL: 'https://discord.com/api/',
+  timeout: 3000,
+  headers: {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+	"Access-Control-Allow-Headers": "Authorization",
+	"Authorization": `Bot ${TOKEN}`
   }
 });
 
-async function main() {
-  const commands = [
-    OrderCommand,
-    RolesCommand,
-    UsersCommand,
-    ChannelsCommand,
-    BanCommand,
-    RegisterCommand,
-  ];
-  try {
-    console.log('Started refreshing application (/) commands.');
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-      body: commands,
-    });
-    client.login(TOKEN);
-  } catch (err) {
-    console.log(err);
-  }
-}
 
-main();
+
+
+app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
+  const interaction = req.body;
+
+  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    console.log(interaction.data.name)
+    if(interaction.data.name == 'yo'){
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `Yo ${interaction.member.user.username}!`,
+        },
+      });
+    }
+
+    if(interaction.data.name == 'dm'){
+      // https://discord.com/developers/docs/resources/user#create-dm
+      let c = (await discord_api.post(`/users/@me/channels`,{
+        recipient_id: interaction.member.user.id
+      })).data
+      try{
+        // https://discord.com/developers/docs/resources/channel#create-message
+        let res = await discord_api.post(`/channels/${c.id}/messages`,{
+          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
+        })
+        console.log(res.data)
+      }catch(e){
+        console.log(e)
+      }
+
+      return res.send({
+        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data:{
+          content:'👍'
+        }
+      });
+    }
+  }
+
+});
+
+
+
+app.get('/register_commands', async (req,res) =>{
+  let slash_commands = [
+    {
+      "name": "yo",
+      "description": "replies with Yo!",
+      "options": []
+    },
+    {
+      "name": "dm",
+      "description": "sends user a DM",
+      "options": []
+    }
+  ]
+  try
+  {
+    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
+    let discord_response = await discord_api.put(
+      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
+      slash_commands
+    )
+    console.log(discord_response.data)
+    return res.send('commands have been registered')
+  }catch(e){
+    console.error(e.code)
+    console.error(e.response?.data)
+    return res.send(`${e.code} error from discord`)
+  }
+})
+
+
+app.get('/', async (req,res) =>{
+  return res.send('Follow documentation ')
+})
+
+
+app.listen(8999, () => {
+
+})
+
